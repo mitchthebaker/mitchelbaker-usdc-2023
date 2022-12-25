2c06b7b3745958822d61ebc9ecdb2a9265bd0ea4
Mitchel Baker <mitchthebaker@Mitchels-MacBook-Pro.local>
2022-12-24 18:30:58 -0800

    implementing first few test cases to verify if arguments passed into  correctly

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # Software Engineering project assessment for the U.S. Digital Corps
 
+### Initial notes from documentation:
+
+1. Organization utilizes chracter recognition service to scan the content of a number of books. 
+- High-level task is to write a function which searches for a word in the scanned book content. 
+
+Write a JS function `findSearchTermInBooks()`
+- two arguments: 
+
+1. a search term (string)
+2. a JSON object containing phrases from books
+
+i.e. 
+[
+  {
+    "Title": string,
+    "ISBN": string,
+    "Content": [
+      {
+        "Page": integer,
+        "Line": integer,
+        "Text": string
+      },
+      ...
+    ]
+  },
+  ...
+]
+
+Note: 
+- JSON object may have zero or more books
+- A book may have zer or more scanned content
+
+- returns: 
+
+1. a JSON object containing lines that match the search word 
+
+i.e. 
+
+{
+  "SearchTerm": string,
+  "Results": [
+    {
+      "ISBN": string,
+      "Page": integer,
+      "Line": integer,
+    },
+    ...
+  ]
+}
+
+Note:
+- returned JSON object may have zero or more results from scanned content 
+
+
+My first thought with JSON data would be to use recursion, but we are not working with nested data. Plus, within "Content" we won't be finding sub-book objects that contain more "Content" objects. Therefore, I think an iterative solution will work well for this problem. 
+
+First, we should always check if the data being passed into our function is correct or not. We should be receiving an array, so lets check for that first. 
+
+Second off, I know that `scannedTextObj` will have zero or many books. This will be our base case. If we have no books, then there are is no content to parse, so we'd simply return:
+
+{
+  "SearchTerm": searchTerm,
+  "Results": []
+}
+
+
---
+++ book_search.js
@@ -0,0 +1,138 @@
+/** 
+ * RECOMMENDATION
+ * 
+ * To test your code, you should open "tester.html" in a web browser.
+ * You can then use the "Developer Tools" to see the JavaScript console.
+ * There, you will see the results unit test execution. You are welcome
+ * to run the code any way you like, but this is similar to how we will
+ * run your code submission.
+ * 
+ * The Developer Tools in Chrome are available under the "..." menu, 
+ * futher hidden under the option "More Tools." In Firefox, they are 
+ * under the hamburger (three horizontal lines), also hidden under "More Tools." 
+ */
+
+/**
+ * Searches for matches in scanned text.
+ * @param {string} searchTerm - The word or term we're searching for. 
+ * @param {JSON} scannedTextObj - A JSON object representing the scanned text.
+ * @returns {JSON} - Search results.
+ * */ 
+ function findSearchTermInBooks(searchTerm, scannedTextObj) {
+  /** You will need to implement your search and 
+   * return the appropriate object here. */
+
+  const result = {
+      "SearchTerm": searchTerm,
+      "Results": []
+  };
+
+  if(!Array.isArray(scannedTextObj)) return result;
+  
+  return result; 
+}
+
+/** Example input object. */
+const twentyLeaguesIn = [
+  {
+      "Title": "Twenty Thousand Leagues Under the Sea",
+      "ISBN": "9780000528531",
+      "Content": [
+          {
+              "Page": 31,
+              "Line": 8,
+              "Text": "now simply went on by her own momentum.  The dark-"
+          },
+          {
+              "Page": 31,
+              "Line": 9,
+              "Text": "ness was then profound; and however good the Canadian\'s"
+          },
+          {
+              "Page": 31,
+              "Line": 10,
+              "Text": "eyes were, I asked myself how he had managed to see, and"
+          } 
+      ] 
+  }
+]
+  
+/** Example output object */
+const twentyLeaguesOut = {
+  "SearchTerm": "the",
+  "Results": [
+      {
+          "ISBN": "9780000528531",
+          "Page": 31,
+          "Line": 9
+      }
+  ]
+}
+
+/*
+_   _ _   _ ___ _____   _____ _____ ____ _____ ____  
+| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___| 
+| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \ 
+| |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
+\___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/ 
+                                                    
+*/
+
+/* We have provided two unit tests. They're really just `if` statements that 
+* output to the console. We've provided two tests as examples, and 
+* they should pass with a correct implementation of `findSearchTermInBooks`. 
+* 
+* Please add your unit tests below.
+* */
+
+/** We can check that, given a known input, we get a known output. */
+const test1result = findSearchTermInBooks("the", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
+  console.log("PASS: Test 1");
+} else {
+  console.log("FAIL: Test 1");
+  console.log("Expected:", twentyLeaguesOut);
+  console.log("Received:", test1result);
+}
+
+/** We could choose to check that we get the right number of results. */
+const test2result = findSearchTermInBooks("the", twentyLeaguesIn); 
+if (test2result.Results.length == 1) {
+  console.log("PASS: Test 2");
+} else {
+  console.log("FAIL: Test 2");
+  console.log("Expected:", twentyLeaguesOut.Results.length);
+  console.log("Received:", test2result.Results.length);
+}
+
+/** 
+ * An array of JSON objects should be passed into `scannedTextObj`. 
+ * If we receive an object, string, number, or null/undefined, 
+ * then the initial `result` should be returned. 
+ */
+const result = {
+  "SearchTerm": "the",
+  "Results": []
+};
+
+// Test when passing in an object for `scannedTextObj`
+const test3result = findSearchTermInBooks("the", {});
+if(JSON.stringify(result) === JSON.stringify(test3result)) {
+  console.log("PASS: Test 3");
+}
+else {
+  console.log("FAIL: Test 3");
+  console.log("Expected:", result);
+  console.log("Received:", test3result);
+}
+
+// Test when passing in a string for `scannedTextObj`
+const test4result = findSearchTermInBooks("the", "Twenty Thousand Leagues Under the Sea");
+if(JSON.stringify(result) === JSON.stringify(test4result)) {
+  console.log("PASS: Test 4");
+}
+else {
+  console.log("FAIL: Test 4");
+  console.log("Expected:", result);
+  console.log("Received:", test4result);
+}
